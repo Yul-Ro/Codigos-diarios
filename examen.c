c3a9f68863634312bc149fbfcbf3bb401e6ed1ba
Yul-Ro <ulissreyesmagos06@gmail.com>
2025-05-13 18:44:57 -0600

    Add files via upload

---
+++ examen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+ int arr 20;
+ int i=0;
+ double suma =0.0;
+ double product=1.0;
+ 
+ printf("introduce 20 numero:\n");
+ 
+ while( i < 20){
+ printf("Elemento %d: " i + 1);
+ scanf("%d", &arr(1));
+ sum += arr(i);
+ product *= arr(i);
+ i++
+ }
+
+duoble promedio sum/ 20;
+printf("\nResultados:\n");
+printf("promedio: %.2lf\n ", promedio);
+printf("Multiplicacion de todos los elementos: %.2lf\n" , product );
+
+}
