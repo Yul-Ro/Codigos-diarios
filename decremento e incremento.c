f836ac650396ac31f9c745e2cef2664a3c69c63a
Yul-Ro <ulissreyesmagos06@gmail.com>
2025-05-20 07:01:25 -0600

    Add files via upload

---
+++ decremento e incremento.c
@@ -0,0 +1,10 @@
+int main (){
+int var = 1;
+int con = 1;
+
+//Es equivalente a var = var+1;
+print ("%d",var++);
+printf("%d",var);
+
+//Es equivalente a con = con-1;
+printf("%")}
---
+++ variables.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{double a = 65;
+double b = 5;
+double c = 6;
+printf("%f", a,b,c);
+    return 0;
+}
