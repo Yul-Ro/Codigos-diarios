2eff09eceae8e51f75c2786fefd73e4e2c4ba93c
Yul-Ro <ulissreyesmagos06@gmail.com>
2025-05-28 20:52:04 -0600

    Add files via upload

---
+++ If_anidado.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main () {
+    int anio;
+    int sala;
+    int por;
+    puts("ingresa tu antiguedad");
+         scanf( "%d", &anio );
+    if ( anio >= 5)
+    printf("Ingrese salario");
+    scanf("%d" , &sala);
+       if ( (sala*100)/10 > 1000) 
+       { printf("felicidades tienes tu credito" );}
+printf( "Fin del programa\n");
+system ("Pause") ;
+return 0;        
+}
---
+++ calificacion .c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+int cal;
+puts("ingresa tu calificacion ");
+scanf("%d" , & cal);
+if (cal >= 8) 
+puts ("Eres un picudo");
+return 0;
+}
